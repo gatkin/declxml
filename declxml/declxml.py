1e59cdfd946e62d465b58f78842d69705fecf0ec
gatkin <greg.scott.atkin@gmail.com>
2017-09-02 13:21:44 -0500

    Add more extensive module documentation

--- declxml/declxml.py
+++ declxml/declxml.py
@@ -1,4 +1,30 @@
-"""Enables XML to be processed using declarative syntax"""
+"""
+declxml is a library for declaratively processing XML documents.
+
+Processors
+---------------
+The declxml library uses processors to define the structure of XML documents. Processors can be
+divided into two categories: *primitve processors* and *aggregate processors*.
+
+Primitive Processors
+----------------------
+Primitive processors are used to parse and serialize simple, primitve values. The following module
+functions are used to construct primitive processors:
+
+.. autofunction:: declxml.boolean
+.. autofunction:: declxml.floating_point
+.. autofunction:: declxml.integer
+.. autofunction:: declxml.string
+
+Aggregate Processors
+---------------------
+Aggregate processors are composed of other processors. These processors are used for values such as
+dictionaries, arrays, and user objects.
+
+.. autofunction:: declxml.array
+.. autofunction:: declxml.dictionary
+.. autofunction:: declxml.user_object
+"""
 import warnings
 import xml.dom.minidom as minidom
 import xml.etree.ElementTree as ET

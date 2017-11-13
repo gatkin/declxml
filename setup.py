b59c66cd86fb9b32632d4865add79cc02eda0567
gatkin <greg.scott.atkin@gmail.com>
2017-11-12 20:28:56 -0600

    Publish version 0.10.0 to PyPi

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 setup(
     name='declxml',
     description='Declarative XML processing library',
-    version='0.9.2',
+    version='0.10.0',
     url='http://declxml.readthedocs.io/',
     author='Greg Atkin',
     author_email='greg.scott.atkin@gmail.com',
     license='MIT',
     py_modules=['declxml'],
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',

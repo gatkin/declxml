d64ab204ececdc01f3334fba7ef8ce79ae4382be
gatkin <gregscottatkin@gmail.com>
2018-09-16 17:47:25 -0500

    Publish to PyPi test on Azure pipelines

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     description='Declarative XML processing library',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='1.1.1',
+    version='1.1.2-rc0',
     url='http://declxml.readthedocs.io/',
     project_urls={
         'Documentation': 'http://declxml.readthedocs.io/',

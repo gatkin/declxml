43de0a630ca7bd0099bff281d1a0cd93a9adef92
gatkin <gregscottatkin@gmail.com>
2018-09-21 21:15:05 -0500

    Only publish to PyPi on tagged commits

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     description='Declarative XML processing library',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='1.1.2-rc0',
+    version='1.1.2-rc1',
     url='http://declxml.readthedocs.io/',
     project_urls={
         'Documentation': 'http://declxml.readthedocs.io/',

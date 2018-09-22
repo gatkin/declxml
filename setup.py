7abd8b8432c13e524211acff95084cb2524be464
gatkin <gregscottatkin@gmail.com>
2018-09-22 09:32:16 -0500

    Rename PyPi service connection

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     license='MIT',
     py_modules=['declxml'],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
